a4f78d61a542bb4e52b7220ab962aa3fe1e0d791
Srini Nariangadu <srini.nariangadu@gmail.com>
2020-07-01 19:49:00 -0700

    Creating shared lib for stockfish engine.

---
+++ src/main.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "bitboard.h"
+#include "endgame.h"
+#include "position.h"
+#include "search.h"
+#include "thread.h"
+#include "tt.h"
+#include "uci.h"
+#include "syzygy/tbprobe.h"
+
+namespace PSQT {
+  void init();
+}
+
