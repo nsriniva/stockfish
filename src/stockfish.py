683d4a949e1fddc6408593976aee38bae6feda14
Srini Nariangadu <srini.nariangadu@gmail.com>
2020-07-01 20:52:56 -0700

    Added __init__ and __del__ methods.

--- src/stockfish.py
+++ src/stockfish.py
@@ -1,2 +1,24 @@
 import cppyy
-cppyy.include('./main.cpp')
+cppyy.include('./main.hpp')
+cppyy.load_library('libstockfish.so')
+
+from cppyy.gbl import engine_info, UCI, Tune, PSQT, Bitboards, \
+    Position, Bitbases, Endgames, Threads, Search, Options
+
+
+class Stockfish(object):
+
+    def __init__(self):
+        UCI.init(Options)
+        Tune.init()
+        PSQT.init()
+        Bitboards.init()
+        Position.init()
+        Bitbases.init()
+        Endgames.init()
+        Threads.set(int(Options['Threads']).__float__())
+        Search.clear()
+
+
+    def __del__(self):
+        Threads.set(0)
