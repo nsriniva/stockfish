ebe2d6a5bf53b93276930835b7988b92ea4ae750
Srini Nariangadu <srini.nariangadu@gmail.com>
2020-07-21 02:22:36 -0700

    Added timing code to move_generator.py

--- src/move_generator.py
+++ src/move_generator.py
@@ -1,13 +1,23 @@
+from datetime import datetime
 from stockfish import UCI, Stockfish
 
 s = Stockfish()
 
-for i in range(1000000):
-    print(f'Iteration {i}')
-    s.position('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
+LOOP_COUNT = 1000000
+POSITION = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
 
-    for m in s.legal_moves():
-        print(UCI.move(m.move, s.is_chess960()), end=' ')
+start = datetime.now()
+for i in range(LOOP_COUNT):
+    print(f'LEGAL Iteration {i}')
+    s.position(POSITION)
 
-    print(s.legal_moves_str(), end=' ')
+    #for m in s.legal_moves():
+    #    print(UCI.move(m.move, s.is_chess960()), end=' ')
 
+    print(f'LEGAL : {s.legal_moves_str()}')
+
+end = datetime.now()
+
+print(f'List of legal moves for position({POSITION}) : {s.legal_moves_str()}')
+
+print(f'Time taken for {LOOP_COUNT} iterations: {end-start}')
